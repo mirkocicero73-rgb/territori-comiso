12c5d066ede7c330c43d1c5ab476a33d95d8a9d7
mirkocicero73-rgb <295649887+mirkocicero73-rgb@users.noreply.github.com>
2026-09-05 16:06:52 +0200

    Niente doppio tocco per lo zoom e tocco immediato, disegno a densita 3x nativa dello schermo iPhone

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 /* Il documento viene sempre chiesto alla rete per primo: così una versione
    nuova (o la schermata di sblocco) compare subito, non al secondo avvio.
    Il resto sta in cache, e l'app funziona anche senza campo. */
-const V = 'territori-comiso-535413b64cee';
-const SHELL = ['./', './index.html', './app.enc?v=535413b64cee', './manifest.webmanifest',
+const V = 'territori-comiso-f3e5293ac260';
+const SHELL = ['./', './index.html', './app.enc?v=f3e5293ac260', './manifest.webmanifest',
                './informativa/', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
